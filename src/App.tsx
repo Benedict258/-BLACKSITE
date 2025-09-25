5bb1ca7b3f66f1c5171d1974f51e56ca990d088b
Benedict258 <isaacbenedict001@gmail.com>
2025-09-25 20:05:07 +0100

    feat(rich-text): add ReactQuill editor (bold/italic/strike/font size/font family), sanitize HTML; render formatted content; prevent media download; fix refresh 404 via HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/lib/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import RoomPage from "./pages/RoomPage";
 import NotFound from "./pages/NotFound";
@@ -21,14 +21,14 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/room/:code" element={<RoomPage />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
